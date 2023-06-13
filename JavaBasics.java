e977d098f46a4fe3ff56b1ea6ea66e1873fe2fc6
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2023-06-13 16:27:41 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("HI");
+        System.out.print("HI\n");
+        System.out.print("HI");
+    }
+    
+}
