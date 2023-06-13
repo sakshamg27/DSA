ff2f81f897d7eddadfd16e213938ab9cbc63caff
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2023-06-13 16:32:25 +0530

    Update JavaBasics.java

--- JavaBasics.java
+++ JavaBasics.java
@@ -2,6 +2,7 @@ public class JavaBasics
 {
     public static void main(String[] args)
     {
+        // new line
         System.out.println("HI");
         System.out.print("HI\n");
         System.out.print("HI");
