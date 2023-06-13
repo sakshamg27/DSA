653422336164c44dbfaa47f9d57e0288075134d2
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2023-06-13 21:02:36 +0530

    Add files via upload

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,11 +1,20 @@
+import java.util.*;
 public class JavaBasics 
 {
     public static void main(String[] args)
     {
-        // new line
+        int a = 10;
+        String name = "Saksham";
+        boolean cg = true;
+
         System.out.println("HI");
         System.out.print("HI\n");
         System.out.print("HI");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String input2 = sc.nextLine();
+        
     }
     
 }
