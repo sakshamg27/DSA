c5f516fce5c638a8d9ea3384cc312e981d42a58e
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2023-06-21 00:51:34 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -9,11 +9,11 @@ public class JavaBasics
         char operator = sc.next().charAt(0);
         switch(operator)
         {
-            case '+': System.out.println(a+b);
+            case '+' : System.out.println(a+b);
                       break;
-            case '-': System.out.println(a-b);
+            case '-' : System.out.println(a-b);
                       break;
-            default: System.out.println("Invalid operator");
+            default : System.out.println("Invalid operator");
         }
     }
     
