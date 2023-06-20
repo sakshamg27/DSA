56f05b57f8dec0fbd390b6e421d0640617578522
sakshamg27 <87021967+sakshamg27@users.noreply.github.com>
2023-06-21 00:48:56 +0530

    Update JavaBasics.java

--- JavaBasics.java
+++ JavaBasics.java
@@ -3,19 +3,19 @@ public class JavaBasics
 {
     public static void main(String[] args)
     {
-        int a = 10;
+        int a = 10;     //initialising
         String name = "Saksham";
         boolean var = true; 
 
-        System.out.println("HI");
+        System.out.println("HI");      //new line 
         System.out.print("HI\n");
         System.out.print("HI");
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);       //taking input
         String input = sc.next();
         String input2 = sc.nextLine();
         
-        int a = 10;
+        int a = 10;        //output
         int b = 10;
         System.out.println("add=" + (a+b)); \\ syntax is imp
         
