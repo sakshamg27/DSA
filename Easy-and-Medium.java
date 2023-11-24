ef2a2d29430c7795fefc3abc2998f2e725c8e522
Saksham Gupta <87021967+sakshamg27@users.noreply.github.com>
2023-11-24 19:18:55 +0530

    Update Easy-and-Medium.java

--- Easy-and-Medium.java
+++ Easy-and-Medium.java
@@ -1,7 +1,7 @@
 import java.util.*;                       
 public class JavaBasics                            
 {              
-    public static void main(String[] args)            
+    public static void main(String[] args)             
     {     
         int a = 10;     //initialising   
         String name = "Saksham";
