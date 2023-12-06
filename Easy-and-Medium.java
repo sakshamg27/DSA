2335c80512be6542628b5c17dd63be77a3bc96bb
Saksham Gupta <87021967+sakshamg27@users.noreply.github.com>
2023-12-06 15:01:11 +0530

    Update Easy-and-Medium.java

--- Easy-and-Medium.java
+++ Easy-and-Medium.java
@@ -9,7 +9,7 @@ public class JavaBasics
 
         System.out.println("HI");      //new line   
         System.out.print("HI\n");
-        System.out.print("HI");
+        System.out.print("HI"); 
 
         Scanner sc = new Scanner(System.in);       //taking input
         String input = sc.next();
