f8d076ffa1b1ba1ff5e89c649624a99110b30824
Saksham Gupta <87021967+sakshamg27@users.noreply.github.com>
2024-01-24 01:34:02 +0530

    Update Easy-and-Medium.java

--- Easy-and-Medium.java
+++ Easy-and-Medium.java
@@ -1,4 +1,4 @@
-import java.util.*;                                  
+import java.util.*;                                   
 public class JavaBasics                                            
 {                             
     public static void main(String[] args)                             
