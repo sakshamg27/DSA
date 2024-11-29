c5c5b623d7ab863befab43483bc090f15b19d931
Saksham Gupta <87021967+sakshamg27@users.noreply.github.com>
2024-11-29 20:37:44 +0530

    Update Easy-and-Medium.java

--- Easy-and-Medium.java
+++ Easy-and-Medium.java
@@ -3,7 +3,7 @@ public class JavaBasics
 {                                        
     public static void main(String[] args)                                     
     {                             
-          
+           
     
 	    
           int a = 10;     //initialising                                 
